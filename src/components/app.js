11ce20065553142860cfe3a1a3b92095f66b9953
Nello Brunelli <nello.brunelli@wyscout.com>
2016-10-26 15:19:58 +0200

    Start working

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import BookList from '../containers/book-list';
+import BookDetail from '../containers/book-detail';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <BookList />
+                <BookDetail />
+            </div>
+        );
+    }
+}
