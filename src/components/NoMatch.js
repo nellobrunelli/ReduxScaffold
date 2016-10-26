70ccb8ddedc2a80fee3da446e988ea9c480122b0
Nello Brunelli <nello.brunelli@wyscout.com>
2016-10-26 16:10:35 +0200

    React Router up and running

---
+++ src/components/NoMatch.js
@@ -0,0 +1,9 @@
+export default class NoMatch extends React.Component {
+    render = () => {
+        return (
+            <h1>
+                {'be carefull man ... I dont know this Route'}
+            </h1>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import App from './components/app';
+import NoMatch from './components/NoMatch';
+
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory} >
+        <Route path="/" component={App} />
+        <Route pathname="*" path="*" component={NoMatch}/>
+    </Router>
   </Provider>
   , document.querySelector('.container'));
